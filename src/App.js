400eebb13002dd1e4ef4c0540cd647d23327f40f
Arjun Raj <arjunraj.pala@gmail.com>
2021-08-31 21:54:36 +0530

    the original files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Weavy from './weavy/Weavy';
+import WeavyApp from './weavy/WeavyApp';
 
-export default App;
+export default class App extends Component {
+  async getJwt() {
+    return 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJteWNsaWVudGlkIiwibmFtZSI6ImFrc2hheSIsInBhc3N3b3JkIjoiYWtzaGF5QDk3Iiwic3ViIjoibXljbGllbnRpZCIsImNsaWVudF9pZCI6Im15Y2xpZW50aWQiLCJjbGllbnRfc2VjcmV0IjoibXljbGllbnRzZWNyZXQiLCJuYmYiOjE2MzA0MjE2MDAsImV4cCI6MTY2MTk1ODc3Mn0.mAZERMOUvpxcmz3B057XZYsH6OYy23VLw7A9xeCHA1s';
+  }
+
+  render() {
+    return (
+      <Weavy jwt={this.getJwt}>
+        <div className="App">
+          <WeavyApp
+            spaceKey="react-space"
+            spaceName="React Space"
+            appKey="react-files"
+            appName="React Files"
+            appType="files"
+            />
+        </div>
+      </Weavy>
+    );
+  }
+}
