bf3261411c28abb05b1d2bec622fae89ee1d4cb4
Arjun Raj <arjunraj.pala@gmail.com>
2021-09-01 11:40:39 +0530

    Route change
    changing routes with the components

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as  Switch, Route } from 'react-router-dom';
 import './index.css';
 import App from './App';
+import digilibrary from './Components/digilibrary';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+<Switch>
+ <Route exact path="/" component={App}/>
+ <Route exact path="/digilibrary" component={digilibrary} />
+</Switch>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
